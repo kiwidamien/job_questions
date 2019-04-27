534e688b9753e487a3da09e1e8855717f7b7f674
kiwidamien <djmartin@ucdavis.edu>
2019-04-26 18:43:41 -0700

    tidied code a little

--- jenn_interview_questions/job_scraper/spotify.py
+++ jenn_interview_questions/job_scraper/spotify.py
@@ -3,41 +3,62 @@ import pandas as pd
 from pprint import pprint
 import time
 
-def download_spotify_jobs_single_page(initial_page=1, per_page=50):
+
+def _make_spotify_job_request(initial_page=1, per_page=50):
     URL = 'https://www.spotifyjobs.com/wp-admin/admin-ajax.php'
+    DATASCIENCE = 343
     payload = {'action': 'get_jobs', 'pageNr': initial_page,
-               'perPage': per_page, 'category':343}
+               'perPage': per_page, 'category': DATASCIENCE}
     response = requests.post(URL, data=payload)
     if response.status_code != 200:
         raise EOFError(f'Returned with response code {response.status_code}')
     return response.json()
 
+
+def _download_spotify_jobs_single_page(initial_page=1, per_page=50):
+    result = _make_spotify_job_request(initial_page, per_page)
+    if 'data' not in result:
+        raise KeyError(f'data not a key in returned result: {pprint(result)}')
+    if 'items' not in result['data']:
+        raise KeyError(f'items not a key in returned result[data]: {pprint(result)}')
+    return result['data']['items']
+
+
 def download_spotify_jobs():
+    """Call to download all spotify jobs.
+
+    Returns nested objects from API call.
+    """
     results = []
     current_page, per_page = 1, 50
     while True:
-        new_responses = download_spotify_jobs_single_page(current_page, per_page)
-        if 'data' in new_responses and 'items' in new_responses['data']:
-            new_jobs = new_responses['data']['items']
-            results.extend(new_jobs)
-            if len(new_jobs) < per_page:
-                return results
-            time.sleep(1)
-            current_page += 1
-            print(f'current page is {current_page}')
-        else:
-            print('keys not in response, exiting')
-            return []
+        print(f'current page is {current_page}')
+        new_jobs = _download_spotify_jobs_single_page(current_page, per_page)
+        results.extend(new_jobs)
+        if len(new_jobs) < per_page:
+            return results
+        time.sleep(1)
+        current_page += 1
+
 
 def simplify_job(job_object):
-    """Flattens information for a single job from spotify (original object nested)"""
-    return {
+    """Flattens information for a single job from spotify.
+
+    Takes the original job object, and returns a dictionary with the following keys:
+    title: string
+    url: string
+    locations: list of strings
+    categories: list of strings
+    is_ny: boolean
+    """
+    flat_job = {
         'title': job_object.get('title'),
         'url': job_object.get('url'),
         'locations': [loc['name'] for loc in job_object.get('locations', [])],
         'categories': [cat['name'] for cat in job_object.get('categories', [])]
     }
-
+    flat_job['is_ny'] = ('New York' in flat_job['locations'])
+    return flat_job
 
 if __name__ == '__main__':
     simple_jobs = [simplify_job(job) for job in download_spotify_jobs()]
